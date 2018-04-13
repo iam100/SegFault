59b76529965ebb4837876733053c026fa486f431
iam100 <32260535+iam100@users.noreply.github.com>
2018-04-14 01:44:50 +0530

    Added Questions and add question

--- app.py
+++ app.py
@@ -31,11 +31,35 @@ app.debug = True
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'SegFault'
+app.config['MYSQL_DB'] = 'segfault'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 # init MySQL
 mysql = MySQL(app)
+
+
+# Registration Form
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('E-Mail', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+# Check if user logged in
+def is_loggedin(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please Login first', 'danger')
+            return redirect(url_for('login'))
+    return wrap
 
 
 # Redirecting to Home page
@@ -43,25 +67,164 @@ mysql = MySQL(app)
 def index():
     return render_template('home.html')
 
+
 # Redirecting to the about page
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-#Redirecting to login page
-@app.route('/login')
+
+# Redirecting to the dashboard
+@app.route('/dashboard')
+@is_loggedin
+def dashboard():
+    return render_template('dashboard.html')
+
+
+# Accessing the user profile
+@app.route('/profile')
+@is_loggedin
+def profile():
+    # cursor
+    cur = mysql.connection.cursor()
+    username = session['username']
+
+    # Get user by username
+    result = cur.execute("SELECT * FROM users where user_username = %s", [username])
+    if result > 0:
+        data = cur.fetchone()
+        name = data['user_name']
+        email = data['user_email']
+        id = data['user_id']
+        date = data['register_date']
+
+    return render_template('profile.html', name=name, email=email, id=id, date=date)
+
+
+# Register page
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # Create a Cursor
+        cur = mysql.connection.cursor()
+
+        cur.execute("INSERT INTO users(user_name, user_email, user_username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        # Commit to db
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+
+        flash('You are now registered and you can login', 'success')
+
+        return redirect(url_for('index'))
+    return render_template('signup.html', form=form)
+
+
+# User login
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        # Get form fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM users where user_username = %s", [username])
+
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['password']
+
+            # Compare Passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # Correct password
+                session['logged_in'] = True
+                session['username'] = username
+
+                # Flash will flash a message
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Wrong Password'
+                return render_template('login.html', error=error)
+
+            # Close the connection to the database
+            cur.close()
+        else:
+            error = 'Username Not Found'
+            return render_template('login.html', error=error)
+
     return render_template('login.html')
 
-#Redirecting to Sign Up
-@app.route('/signup')
-def singup():
-    return render_template('signup.html')
+
+# Logout
+@app.route('/logout')
+@is_loggedin
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+
+# Question Class
+class QuestionForm(Form):
+    statement = StringField('Question', [validators.Length(min=1, max=280)])
+    body = TextAreaField('Description', [validators.Length(max=500)])
+
+
+# Add Question Page
+@app.route('/addquestion', methods=['GET', 'POST'])
+@is_loggedin
+def addquestion():
+    form = QuestionForm(request.form)
+    if request.method == 'POST' and form.validate():
+        statement = form.statement.data
+        body = form.body.data
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        cur.execute("INSERT INTO questions(statement,body,poster) VALUES(%s, %s, %s)",(statement, body, session['username']))
+
+        mysql.connection.commit()
+        cur.close()
+        flash('Question Posted', 'success')
+        return redirect(url_for('dashboard'))
+    return render_template('addquestion.html', form=form)
+
+
+@app.route('/questions')
+def question():
+    cur = mysql.connection.cursor()
+
+
+    result = cur.execute("SELECT * FROM questions ORDER BY id DESC")
+
+
+    qs = cur.fetchall()
+    if result > 0 :
+        return render_template('questions.html', qs=qs)
+    else :
+        msg = "No article found"
+        return render_template('questions.html',msg=msg)
+    cur.close()
+
 
 # Running the app if app.py is the main module
 if __name__ == '__main__':
     # Encryption Key
-    app.secret_key = 'secret123'
+    app.secret_key='bZ\x85\xb2\xfc1$\xe6\n\xa1\xc0\xce\xdd\x9f\x815\xc0\xe4\xac\xc6\xfc\x0e\xa9\xa0V'
 
     # Starting the app
     app.run()
