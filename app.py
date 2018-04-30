ae13a6288ba32d598f34da7c8743657941f97966
iam100 <32260535+iam100@users.noreply.github.com>
2018-04-30 18:16:45 +0530

    Final Change Before Eval

--- app.py
+++ app.py
@@ -24,13 +24,15 @@ from passlib.hash import sha256_crypt
 # Imports from Functools
 from functools import wraps
 
+from math import floor
+
 app = Flask(__name__)
 app.debug = True
 
 # MySQL config
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = 'Anush@1510'
 app.config['MYSQL_DB'] = 'segfault'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
@@ -113,16 +115,37 @@ def about():
 @app.route('/dashboard')
 @is_loggedin
 def dashboard():
-    return render_template('dashboard.html')
+    username = session['username']
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Get questions
+    results = cur.execute("SELECT * FROM questions where poster = %s", [username])
+
+    questions = cur.fetchall()
+    # Get user by username
+    result = cur.execute("SELECT * FROM users where user_username = %s", [username])
+    if result > 0:
+        data = cur.fetchone()
+        name = data['user_name']
+
+    if results>0:
+        return render_template('dashboard.html', questions=questions, name=name)
+    else:
+        msg = "No Questions Asked Yet"
+        return render_template('dashboard.html', msg=msg, name=name)
 
 
 # Accessing the user profile
 @app.route('/profile')
 @is_loggedin
 def profile():
-    # cursor
+ # cursor
     cur = mysql.connection.cursor()
     username = session['username']
+    no_questions = 0
+    no_answers = 0
+
 
     # Get user by username
     result = cur.execute("SELECT * FROM users where user_username = %s", [username])
@@ -133,8 +156,15 @@ def profile():
         id = data['user_id']
         date = data['register_date']
 
-    return render_template('profile.html', name=name, email=email, id=id, date=date)
+    result = cur.execute("SELECT * FROM questions WHERE poster = %s", [username])
+    if result > 0:
+        no_questions = result
 
+    result = cur.execute("SELECT * FROM answers WHERE author = %s", [username])
+    if result > 0:
+        no_answers = result
+
+    return render_template('profile.html', name=name, email=email, id=id, date=date, no_questions=no_questions, no_answers=no_answers)
 
 # Register page
 @app.route('/signup', methods=['GET', 'POST'])
@@ -238,88 +268,269 @@ def addquestion():
         return redirect(url_for('dashboard'))
     return render_template('addquestion.html', form=form)
 
-
-@app.route('/questions')
-def question():
+@app.route('/questions',defaults={'id':1})
+@app.route('/questions/<int:id>/')
+def question(id):
     cur = mysql.connection.cursor()
-
-
     result = cur.execute("SELECT * FROM questions ORDER BY id DESC")
-
-
+    skip = (id-1)*10
+    last = floor(result/10)
+    if last == 0:
+        last  = 1
+    cur.execute("SELECT * FROM questions ORDER BY id DESC LIMIT %s,%s",(skip,10))
     qs = cur.fetchall()
+    print(last)
+    if id > last :
+        return redirect(url_for('question',id = last))
     if result > 0:
-        return render_template('questions.html', qs=qs)
+        return render_template('questions.html',last=last, qs=qs,id=id)
     else:
-        msg = "No article found"
-        return render_template('questions.html',msg=msg)
+        msg = "No questions found"
+        return render_template('questions.html',msg=msg,id=id)
     cur.close()
 
+@app.route('/questions/answered',defaults={'id':1})
+@app.route('/questions/answered/<int:id>/')
+def answered_question(id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM questions ORDER BY id DESC")
+    answered = []
+    for row in cur:
+        cur2 = mysql.connection.cursor()
+        cur2.execute("SELECT statement FROM answers,questions WHERE qid = %s",[row['id']])
+        answered.append(cur2.fetchone())
+        cur2.close()
+    result = len(answered)
+    skip = (id-1)*10
+    last = floor(result/10)
+    if last == 0:
+        last  = 1
+    cur.execute("SELECT * FROM answers ORDER BY qid DESC")
+    qs = []
+    previd = ""
+    for row in cur:
+        if previd != row['qid']:
+            cur2 = mysql.connection.cursor()
+            cur2.execute("SELECT * FROM questions WHERE id = %s LIMIT %s,%s",(row['qid'],skip,10))
+            qs.append(cur2.fetchone())
+            previd=row['qid']
+            cur2.close()
+    for quest in qs :
+        print(quest)
+    if id > last :
+        return redirect(url_for('answered_question',id = last))
+    if result > 0:
+        return render_template('answered_questions.html',last=last, qs=qs,id=id,answered=answered)
+    else:
+        msg = "No questions found"
+        return render_template('answered_questions.html',msg=msg,id=id,answered=answered)
+    cur.close()
+
+@app.route('/questions/unanswered',defaults={'id':1})
+@app.route('/questions/unanswered/<int:id>/')
+def unanswered_question(id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM questions ORDER BY id DESC")
+    unanswered = []
+    for row in cur:
+        cur2 = mysql.connection.cursor()
+        cur2.execute("SELECT statement FROM answers,questions WHERE qid != %s",[row['id']])
+        unanswered.append(cur2.fetchone())
+        cur2.close()
+    result = len(unanswered)
+    skip = (id-1)*10
+    last = floor(result/10)
+    if last == 0:
+        last  = 1
+    cur.execute("SELECT * FROM answers ORDER BY qid DESC")
+    qs1 = []
+    previd = ""
+    for row in cur:
+        cur2 = mysql.connection.cursor()
+        cur2.execute("SELECT * FROM questions WHERE id != %s LIMIT %s,%s",(row['qid'],skip,10))
+        qs1.append(cur2.fetchone())
+        previd=row['qid']
+        cur2.close()
+    qs = list({v['id']:v for v in qs1}.values())
+    if id > last :
+        return redirect(url_for('unanswered_question',id = last))
+    if result > 0:
+        return render_template('unanswered_questions.html',last=last, qs=qs,id=id,unanswered=unanswered)
+    else:
+        msg = "No questions found"
+        return render_template('unanswered_questions.html',msg=msg,id=id,unanswered=unanswered)
+    cur.close()
+
+@app.route('/editquestion/<string:id>',methods = ['GET','POST'])
+@is_loggedin
+def editquestion(id):
+    cur2 = mysql.connection.cursor()
+
+    result = cur2.execute("SELECT * FROM questions WHERE id = %s AND poster = %s",([id],[session['username']]))
+    one_qs = cur2.fetchone()
+
+    cur2.close()
+
+    form = QuestionForm(request.form)
+    form.statement.data = one_qs['statement']
+    form.body.data = one_qs['body']
+
+    if request.method == 'POST' and form.validate :
+        body = request.form['body']
+        statement = request.form['statement']
+
+        cur=mysql.connection.cursor()
+
+        cur.execute("UPDATE questions SET body=%s,statement=%s WHERE id = %s ",(body,statement,[id]))
+        mysql.connection.commit()
+        cur.close()
+
+        flash ('Question Updated','success')
+        return redirect(url_for('questions',id = id))
+    return render_template('editquestion.html',form=form,one_qs=one_qs)
+
+
+# Delete questions
+@app.route('/delete_question/<string:id>', methods=['POST'])
+@is_loggedin
+def delete_question(id):
+    # Create a cursor
+    cur = mysql.connection.cursor()
+
+    # Execute Cursor
+    cur.execute("DELETE FROM questions where id=%s", [id])
+
+    mysql.connection.commit()
+    cur.close()
+
+    flash ('Question Deleted','success')
+    return redirect(url_for('dashboard'))
+
+@app.route('/delete_answer/<string:aid>/<int:qid>',methods=['GET','POST'])
+@is_loggedin
+def delete_answer(aid,qid):
+    cur = mysql.connection.cursor()
+
+    # Execute Cursor
+    cur.execute("DELETE FROM answers where id=%s", [aid])
+
+    mysql.connection.commit()
+    cur.close()
+
+    flash ('Answer Deleted','success')
+    return redirect(url_for('questions',id = qid ))
 
 # Comment form
 class CommentForm(Form):
-    body = TextAreaField('Comment',[validators.Length(max=80)])
+    body = TextAreaField('',[validators.Length(min=1,max=80)])
 
-@app.route('/question/<string:id>/',methods = ['GET','POST'])
+
+@app.route('/question/<string:id>/', methods=['GET', 'POST'])
 def questions(id):
-    form = CommentForm(request.form)
-    if request.method == 'POST' and form.validate():
-        body = form.body.data
-
-        cur = mysql.connection.cursor()
-        print("WORKING")
-        cur.execute("INSERT INTO comments(ansid,body,author) VALUES(%s, %s, %s)",( answer['id'],body, session['username']))
-
-        mysql.connection.commit()
-        cur.close()
-        print("RANDOM SHIT")
-        flash('Comment Posted', 'success')
-        return redirect(url_for('question'))
-
-
     cur = mysql.connection.cursor()
 
-    if not 'logged_in' in session :
+    if 'logged_in' not in session:
         username = None
+        uid = 0
     else:
         username = session['username']
+        cur.execute("SELECT * FROM users WHERE user_username = %s",[username])
+        us = cur.fetchone()
+        uid = us['user_id']
 
     cur.execute("SELECT * FROM questions WHERE id = %s",[id])
     one_qs = cur.fetchone()
     cur.close()
 
     cur = mysql.connection.cursor()
-    auths = cur.execute("SELECT * FROM answers WHERE (author,qid) = (%s,%s)",([session['username']],[id]))
+    auths = cur.execute("SELECT * FROM answers WHERE (author,qid) = (%s,%s)",([username],[id]))
     cur.close()
 
     cur2 = mysql.connection.cursor()
 
-    result = cur2.execute("SELECT * FROM answers WHERE qid = %s ORDER BY id DESC",[id])
+    result = cur2.execute("SELECT * FROM answers WHERE qid = %s ORDER BY upvote DESC",[id])
     answers = cur2.fetchall()
+    for answer in answers:
+        print(answer)
     cur2.close()
 
     comments = []
+    #comments_author = []
+    #comments_date = []
 
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM answers WHERE qid = %s ORDER BY id DESC",[id])
+    cur.execute("SELECT * FROM answers WHERE qid = %s ORDER BY upvote DESC",[id])
     for row in cur:
         cur2 = mysql.connection.cursor()
         cur2.execute("SELECT * FROM comments WHERE ansid = %s",[row['id']])
         comments.append(cur2.fetchall())
+        #cur2.execute("SELECT author FROM comments WHERE ansid = %s",[row['id']])
+        #comments_author.append(cur2.fetchall())
+        #cur2.execute("SELECT comDate FROM comments WHERE ansid = %s",[row['id']])
+        #comments_date.append(cur2.fetchall())
         cur2.close()
     cur.close()
-
-
+    form1 = CommentForm(request.form)
+    if request.method == 'POST':
+        answerid = request.form['idd']
+        if form1.validate() :
+            body = form1.body.data
+            cur3 = mysql.connection.cursor()
+            cur3.execute("INSERT INTO comments(ansid,body,author) VALUES(%s, %s, %s)",([answerid],[body], session['username']))
+            mysql.connection.commit()
+            cur3.close()
+            flash('Comment Posted', 'success')
+            return redirect(url_for('questions',id=id))
+            #print("Hello")
+            #cur = mysql.connection.cursor()
+            #cur.execute("SELECT upvote FROM answers WHERE ansid = %s",[answerid])
+            #uvote = cur.fetchone()['upvote'];
+            #uvote += 1
+            #cur.execute("INSERT INTO answers(upvote) VALUES(%d)",(uvote))
+            #mysql.connection.commit()
+            #cur.close()
+    ups = 0
     if result > 0:
-        return render_template('question.html', form=form, one_qs=one_qs, answers=answers, username=username, auths=auths, comments=comments)
+        return render_template('question.html',no_of_up=ups,uid=uid, form1=form1,one_qs=one_qs, answers=answers, username=username, auths=auths,comments=comments)
     else:
         msg = "Not Answered Yet"
-        return render_template('question.html', form=form, one_qs=one_qs, msg=msg, username=username, auths=auths, comments=comments)
+        return render_template('question.html',no_of_up=ups,uid=uid, form1=form1,one_qs=one_qs, msg=msg, username=username, auths=auths,comments=comments)
 
+@app.route('/upvote/<string:user_id>/<string:q_id>/<int:ans_id>/')
+@is_loggedin
+def upvote(user_id,q_id,ans_id):
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM answers WHERE id = %s",[ans_id])
+    if result == 0:
+        abort(404)
+    post = cur.fetchone()
+    result = cur.execute("SELECT * FROM votes WHERE userid = %s AND ansid = %s",([user_id],[ans_id]))
+    cur.close()
+    if result > 0:
+        cur = mysql.connection.cursor()
+        cur.execute("DELETE FROM votes WHERE userid = %s AND ansid = %s",([user_id],[ans_id]))
+        mysql.connection.commit()
+        number = cur.execute("SELECT * FROM votes WHERE ansid = %s",[ans_id])
+        cur.execute("UPDATE answers SET upvote = %s WHERE id = %s",(number,ans_id))
+        mysql.connection.commit()
+        cur.close()
+        flash("Upvote Removed","danger")
+        return redirect(url_for('questions',id=q_id))
+    else :
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO votes(ansid,userid) VALUES(%s,%s)",([ans_id],[user_id]))
+        mysql.connection.commit()
+        number = cur.execute("SELECT * FROM votes WHERE ansid = %s",[ans_id])
+        print(type((ans_id)))
+        cur.execute("UPDATE answers SET upvote = %s WHERE id = %s",(number,ans_id))
+        mysql.connection.commit()
+        cur.close()
+        flash("Upvoted","success")
+        return redirect(url_for('questions',id=q_id))
+    return redirect(url_for('questions',id=q_id))
 
 class AnswerForm(Form):
     body = TextAreaField('Your Answer:',[validators.Length(min=5)])
-
 
 @app.route('/addanswer/<string:id>', methods=['GET', 'POST'])
 @is_loggedin
@@ -344,6 +555,39 @@ def addanswer(id):
         flash('Question Answered', 'success')
         return redirect(url_for('dashboard'))
     return render_template('addanswer.html', form=form ,one_qs=one_qs)
+
+
+@app.route('/editanswer/<string:id>',methods = ['GET','POST'])
+@is_loggedin
+def editanswer(id):
+    form = AnswerForm(request.form)
+    cur2 = mysql.connection.cursor()
+
+    result = cur2.execute("SELECT * FROM questions WHERE id = %s",[id])
+    one_qs = cur2.fetchone()
+
+    cur2.close()
+
+
+    cur = mysql.connection.cursor()
+
+    cur.execute("SELECT * FROM answers WHERE qid = %s AND author = %s ",([id],[session['username']]))
+    one_ans = cur.fetchone()
+
+    form.body.data = one_ans['body']
+
+    if request.method == 'POST' and form.validate :
+        body = request.form['body']
+
+        cur=mysql.connection.cursor()
+
+        cur.execute("UPDATE answers SET body=%s WHERE author = %s",(body,session['username']))
+        mysql.connection.commit()
+        cur.close()
+
+        flash ('Answer Updated','success')
+        return redirect(url_for('question'))
+    return render_template('editanswer.html',form=form,one_qs=one_qs)
 
 
 # Running the app if app.py is the main module
