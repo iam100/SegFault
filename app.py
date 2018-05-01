8a69909270b642f1a3299b52d4b89ee6648f8525
iam100 <32260535+iam100@users.noreply.github.com>
2018-05-01 22:56:36 +0530

    Fixed app.py indentation

--- app.py
+++ app.py
@@ -67,13 +67,15 @@ def is_loggedin(f):
 # Redirecting to Home page
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    friends = []
+
     cur = mysql.connection.cursor()
     cur.execute("SELECT statement FROM questions")
-    friends = []
+
     for row in cur:
         friends.append(row['statement'])
-    print(friends)
     cur.close()
+
     if request.method == 'POST':
         # Get form fields
         username = request.form['username']
@@ -178,17 +180,23 @@ def search():
     strin=request.form['search']
     results = []
     results2 = []
-    results3= []
+    results3 = []
+
     cur = mysql.connection.cursor()
     query  = "'%"+strin+"%'"
+
     no = cur.execute("SELECT * FROM questions WHERE statement LIKE "+query)
     for row in cur:
         results.append(row)
     no2 = cur.execute("SELECT * FROM users WHERE user_username LIKE"+query)
+
     for row in cur:
         results2.append(row)
     cur.close()
+
     return render_template("search.html",results=results,results2=results2,no2=no2,no=no)
+
+
 # Register page
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -262,7 +270,9 @@ def login():
 @is_loggedin
 def logout():
     session.clear()
+
     flash('You are now logged out', 'success')
+
     return redirect(url_for('login'))
 
 
@@ -291,18 +301,22 @@ def addquestion():
         return redirect(url_for('dashboard'))
     return render_template('addquestion.html', form=form)
 
+
 @app.route('/questions',defaults={'id':1})
 @app.route('/questions/<int:id>/')
 def question(id):
     cur = mysql.connection.cursor()
     result = cur.execute("SELECT * FROM questions ORDER BY id DESC")
+
     skip = (id-1)
     last = floor(result)
+
     if last == 0:
         last  = 1
+
     cur.execute("SELECT * FROM questions ORDER BY id DESC LIMIT %s,%s",(skip,1))
     qs = cur.fetchall()
-    print(last)
+
     if id > last :
         return redirect(url_for('question',id = last))
     if result > 0:
@@ -310,27 +324,36 @@ def question(id):
     else:
         msg = "No questions found"
         return render_template('questions.html',msg=msg,id=id)
+
     cur.close()
+
 
 @app.route('/questions/answered',defaults={'id':1})
 @app.route('/questions/answered/<int:id>/')
 def answered_question(id):
+    answered = []
+
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM questions ORDER BY id DESC")
-    answered = []
+
     for row in cur:
         cur2 = mysql.connection.cursor()
         cur2.execute("SELECT statement FROM answers,questions WHERE qid = %s",[row['id']])
         answered.append(cur2.fetchone())
         cur2.close()
+
     result = len(answered)
     skip = (id-1)
     last = floor(result)
+
     if last == 0:
         last  = 1
+
     cur.execute("SELECT * FROM answers ORDER BY qid DESC")
     qs = []
+
     previd = ""
+
     for row in cur:
         if previd != row['qid']:
             cur2 = mysql.connection.cursor()
@@ -338,8 +361,7 @@ def answered_question(id):
             qs.append(cur2.fetchone())
             previd=row['qid']
             cur2.close()
-    for quest in qs :
-        print(quest)
+
     if id > last :
         return redirect(url_for('answered_question',id = last))
     if result > 0:
@@ -347,34 +369,44 @@ def answered_question(id):
     else:
         msg = "No questions found"
         return render_template('answered_questions.html',msg=msg,id=id,answered=answered)
+
     cur.close()
+
 
 @app.route('/questions/unanswered',defaults={'id':1})
 @app.route('/questions/unanswered/<int:id>/')
 def unanswered_question(id):
+    unanswered = []
+
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM questions ORDER BY id DESC")
-    unanswered = []
+
     for row in cur:
         cur2 = mysql.connection.cursor()
         cur2.execute("SELECT statement FROM answers,questions WHERE qid != %s",[row['id']])
         unanswered.append(cur2.fetchone())
         cur2.close()
+
     result = len(unanswered)
     skip = (id-1)*1
     last = floor(result)
+
     if last == 0:
         last  = 1
+
     cur.execute("SELECT * FROM answers ORDER BY qid DESC")
     qs1 = []
     previd = ""
+
     for row in cur:
         cur2 = mysql.connection.cursor()
         cur2.execute("SELECT * FROM questions WHERE id != %s LIMIT %s,%s",(row['qid'],skip,1))
         qs1.append(cur2.fetchone())
         previd=row['qid']
         cur2.close()
+
     qs = list({v['id']:v for v in qs1}.values())
+
     if id > last :
         return redirect(url_for('unanswered_question',id = last))
     if result > 0:
@@ -382,7 +414,9 @@ def unanswered_question(id):
     else:
         msg = "No questions found"
         return render_template('unanswered_questions.html',msg=msg,id=id,unanswered=unanswered)
+
     cur.close()
+
 
 @app.route('/editquestion/<string:id>',methods = ['GET','POST'])
 @is_loggedin
@@ -429,6 +463,7 @@ def delete_question(id):
     flash ('Question Deleted','success')
     return redirect(url_for('dashboard'))
 
+
 @app.route('/delete_answer/<string:aid>/<int:qid>',methods=['GET','POST'])
 @is_loggedin
 def delete_answer(aid,qid):
@@ -473,26 +508,22 @@ def questions(id):
 
     result = cur2.execute("SELECT * FROM answers WHERE qid = %s ORDER BY upvote DESC",[id])
     answers = cur2.fetchall()
-    for answer in answers:
-        print(answer)
+
     cur2.close()
 
     comments = []
-    #comments_author = []
-    #comments_date = []
 
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM answers WHERE qid = %s ORDER BY upvote DESC",[id])
+
     for row in cur:
         cur2 = mysql.connection.cursor()
         cur2.execute("SELECT * FROM comments WHERE ansid = %s",[row['id']])
         comments.append(cur2.fetchall())
-        #cur2.execute("SELECT author FROM comments WHERE ansid = %s",[row['id']])
-        #comments_author.append(cur2.fetchall())
-        #cur2.execute("SELECT comDate FROM comments WHERE ansid = %s",[row['id']])
-        #comments_date.append(cur2.fetchall())
         cur2.close()
+
     cur.close()
+
     form1 = CommentForm(request.form)
     if request.method == 'POST':
         answerid = request.form['idd']
@@ -502,16 +533,10 @@ def questions(id):
             cur3.execute("INSERT INTO comments(ansid,body,author) VALUES(%s, %s, %s)",([answerid],[body], session['username']))
             mysql.connection.commit()
             cur3.close()
+
             flash('Comment Posted', 'success')
             return redirect(url_for('questions',id=id))
-            #print("Hello")
-            #cur = mysql.connection.cursor()
-            #cur.execute("SELECT upvote FROM answers WHERE ansid = %s",[answerid])
-            #uvote = cur.fetchone()['upvote'];
-            #uvote += 1
-            #cur.execute("INSERT INTO answers(upvote) VALUES(%d)",(uvote))
-            #mysql.connection.commit()
-            #cur.close()
+
     ups = 0
     if result > 0:
         return render_template('question.html',no_of_up=ups,uid=uid, form1=form1,one_qs=one_qs, answers=answers, username=username, auths=auths,comments=comments)
@@ -519,41 +544,60 @@ def questions(id):
         msg = "Not Answered Yet"
         return render_template('question.html',no_of_up=ups,uid=uid, form1=form1,one_qs=one_qs, msg=msg, username=username, auths=auths,comments=comments)
 
+
 @app.route('/upvote/<string:user_id>/<string:q_id>/<int:ans_id>/')
 @is_loggedin
 def upvote(user_id,q_id,ans_id):
     cur = mysql.connection.cursor()
     result = cur.execute("SELECT * FROM answers WHERE id = %s",[ans_id])
+
     if result == 0:
         abort(404)
     post = cur.fetchone()
     result = cur.execute("SELECT * FROM votes WHERE userid = %s AND ansid = %s",([user_id],[ans_id]))
     cur.close()
+
     if result > 0:
         cur = mysql.connection.cursor()
+
         cur.execute("DELETE FROM votes WHERE userid = %s AND ansid = %s",([user_id],[ans_id]))
+
         mysql.connection.commit()
+
         number = cur.execute("SELECT * FROM votes WHERE ansid = %s",[ans_id])
+
         cur.execute("UPDATE answers SET upvote = %s WHERE id = %s",(number,ans_id))
+
         mysql.connection.commit()
+
         cur.close()
+
         flash("Upvote Removed","danger")
+
         return redirect(url_for('questions',id=q_id))
     else :
         cur = mysql.connection.cursor()
+
         cur.execute("INSERT INTO votes(ansid,userid) VALUES(%s,%s)",([ans_id],[user_id]))
+
         mysql.connection.commit()
+
         number = cur.execute("SELECT * FROM votes WHERE ansid = %s",[ans_id])
-        print(type((ans_id)))
+
         cur.execute("UPDATE answers SET upvote = %s WHERE id = %s",(number,ans_id))
         mysql.connection.commit()
+
         cur.close()
+
         flash("Upvoted","success")
+
         return redirect(url_for('questions',id=q_id))
     return redirect(url_for('questions',id=q_id))
 
+
 class AnswerForm(Form):
     body = TextAreaField('Your Answer:',[validators.Length(min=5)])
+
 
 @app.route('/addanswer/<string:id>', methods=['GET', 'POST'])
 @is_loggedin
